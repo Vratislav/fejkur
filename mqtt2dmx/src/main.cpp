0bb6011c95e102061c370a5a253b8175c09e175f
Filip Kirschner <filip.kirschner@applifting.cz>
2025-07-22 21:00:31 +0200

    Version for Arduino R3

--- mqtt2dmx/src/main.cpp
+++ mqtt2dmx/src/main.cpp
@@ -1,40 +1,61 @@
-#include <Arduino.h>
-#include <ESPDMX.h>
 
-DMXESPSerial dmx;
-uint8_t channel_values[4] = {0, 0, 0, 0};
+#include <Conceptinetics.h>
+
+#define DMX_CHANNELS 3
+#define RXEN_PIN 2
+#define HOLD_TIME 10000    // 10 seconds in milliseconds
+#define FADE_TIME 3000     // 3 seconds in milliseconds
+#define FADE_STEPS 100     // Number of steps for smooth fade
+
+DMX_Master dmx_master(DMX_CHANNELS, RXEN_PIN);
+
+// Track current RGB values
+int currentR = 0;
+int currentG = 0;
+int currentB = 0;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) { delay(10); }
-  Serial.println("DMX Debug Start");
-  dmx.init();
-  delay(200);
+  dmx_master.enable();
+}
+
+void fadeToColor(int targetR, int targetG, int targetB) {
+  for (int step = 0; step <= FADE_STEPS; step++) {
+    int r = map(step, 0, FADE_STEPS, currentR, targetR);
+    int g = map(step, 0, FADE_STEPS, currentG, targetG);
+    int b = map(step, 0, FADE_STEPS, currentB, targetB);
+    
+    dmx_master.setChannelValue(1, r);
+    dmx_master.setChannelValue(2, g);
+    dmx_master.setChannelValue(3, b);
+    
+    delay(FADE_TIME / FADE_STEPS);
+  }
+  
+  // Update current values
+  currentR = targetR;
+  currentG = targetG;
+  currentB = targetB;
 }
 
 void loop() {
-  for (int ch = 1; ch <= 4; ch++) {
-    // Set all channels to 0
-    for (int i = 1; i <= 4; i++) {
-      dmx.write(i, 0);
-      channel_values[i-1] = 0;
-    }
-    // Set current channel to 255
-    dmx.write(ch, 255);
-    channel_values[ch-1] = 255;
-    dmx.update();
-    // Debug print
-    Serial.print("Cycle: channel ");
-    Serial.print(ch);
-    Serial.print(" = 255, others = 0 | Values: ");
-    for (int i = 0; i < 4; i++) {
-      Serial.print("CH");
-      Serial.print(i+1);
-      Serial.print(":");
-      Serial.print(channel_values[i]);
-      Serial.print(" ");
-    }
-    Serial.println();
-    delay(500);
-  }
+  // Red
+  fadeToColor(255, 0, 0);
+  delay(HOLD_TIME);
+  
+  // Fade to black
+  fadeToColor(0, 0, 0);
+  
+  // Green
+  fadeToColor(0, 255, 0);
+  delay(HOLD_TIME);
+  
+  // Fade to black
+  fadeToColor(0, 0, 0);
+  
+  // Blue
+  fadeToColor(0, 0, 255);
+  delay(HOLD_TIME);
+  
+  // Fade to black
+  fadeToColor(0, 0, 0);
 }

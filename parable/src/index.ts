900d15560d9caee37311f46c4c47516b7b85d42a
Vratislav Kalenda <v.kalenda@gmail.com>
2025-07-30 23:46:29 +0200

    chore: init

---
+++ parable/src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hello, TypeScript Node.js!");
+
+// Example function to demonstrate TypeScript
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+const message = greet("Worldd");
+console.log(message);

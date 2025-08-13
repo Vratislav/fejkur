f3d0c23531592b8db372cd22672a5fcab61314c2
Vratislav Kalenda <v.kalenda@gmail.com>
2025-08-13 19:39:25 +0200

    feat: llm

---
+++ parable/src/llm.ts
@@ -0,0 +1,77 @@
+import googleAI from "@genkit-ai/googleai";
+import { readFile } from "fs/promises";
+import { genkit, z } from "genkit";
+import { text } from "stream/consumers";
+
+// Initialize Genkit with the Google AI plugin
+const ai = genkit({
+  plugins: [googleAI()],
+  model: googleAI.model("gemini-2.5-flash", {
+    temperature: 0.8,
+  }),
+});
+
+export const playerIdentificationSchema = z.object({
+  appareance: z.string(),
+  activity: z.string(),
+  gender: z.enum(["male", "female"]),
+  holdingInHands: z.string().optional(),
+});
+
+export type PlayerIdentification = typeof playerIdentificationSchema._type;
+
+const playerIdentificationPrompt = `
+Describe the players in the room. 
+
+Identify their appearance.
+- What are they wearing?
+- What hair color do they have?
+- Is their hair long or short?
+- Are they male or female?
+- What is their age?
+- What is their gender?
+
+Example appearance: "An older tall man with short brown hair and grey beard, wearing a blue shirt and jeans."
+
+Describe their activity.
+- What are they doing?
+
+Example activity: "The man is standing in the middle of the room, looking at the camera."
+
+Describe what they are holding in their hands.
+- What are they holding?
+
+Example holding: "The man is holding a blue book."
+Example holding when nothing is in their hands: undefined
+
+Describe their gender.
+Example gender: "female"
+
+`;
+
+export const recipeGeneratorFlow = ai.defineFlow(
+  {
+    name: "playerIdentificationFlow",
+
+    inputSchema: z.object({
+      framePath: z.string(),
+    }),
+    outputSchema: z.array(playerIdentificationSchema),
+  },
+  async (input) => {
+    const frame = await frameToLLMInput(input.framePath);
+    const response = await ai.generate({
+      system: playerIdentificationPrompt,
+      prompt: [frame],
+    });
+    console.log(response);
+    return response.data;
+  }
+);
+
+export async function frameToLLMInput(framePath: string) {
+  const data = await readFile(framePath);
+  return {
+    media: { url: `data:image/jpeg;base64,${data.toString("base64")}` },
+  };
+}
